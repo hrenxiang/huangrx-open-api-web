1ad798e11be0ae1a437d5cd8e8d8cc33ae815e87
huangrx <2295701930@qq.com>
2024-02-02 21:45:46 +0800

    feat: 构建详情页面 - 2 🧀

--- src/pages/ApiList/components/Detail.tsx
+++ src/pages/ApiList/components/Detail.tsx
@@ -1,64 +1,27 @@
-import { PageContainer, ProCard, ProDescriptions } from '@ant-design/pro-components';
+import { PageContainer, ProCard, ProDescriptions, ProTable } from '@ant-design/pro-components';
 import React from 'react';
-import { useLocation } from 'react-router';
 import { Divider } from 'antd';
+import { useLocation } from 'react-router-dom';
+import '../index.less';
 
 const Detail: React.FC = () => {
   let location = useLocation();
   let { data } = location.state;
 
+  const titleStyle = {
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+    flex: 'auto',
+    color: 'rgba(0, 0, 0, 0.88)',
+    fontWeight: 600,
+    fontSize: '16px',
+    lineHeight: 1.5,
+  };
+
   return (
     <PageContainer>
       <ProCard boxShadow>
-        <ProDescriptions
-          title="简介"
-          request={async () => {
-            return Promise.resolve({
-              success: true,
-              data: data,
-            });
-          }}
-          style={{ marginBottom: '32px' }}
-          emptyText={'空'}
-          columns={[
-            {
-              title: 'API端点名称',
-              dataIndex: 'name',
-            },
-            {
-              title: 'API端点描述',
-              dataIndex: 'description',
-              valueType: 'textarea',
-            },
-            {
-              title: '调用频率限制',
-              dataIndex: 'rateLimit',
-              hideInForm: true,
-              hideInSearch: true,
-            },
-            {
-              title: '状态',
-              dataIndex: 'status',
-              hideInForm: true,
-              valueEnum: {
-                0: {
-                  text: '未生效',
-                  status: 'Default',
-                },
-                1: {
-                  text: '生效中',
-                  status: 'Processing',
-                },
-              },
-            },
-            {
-              title: '创建时间',
-              dataIndex: 'updateTime',
-              valueType: 'dateTime',
-            },
-          ]}
-        ></ProDescriptions>
-        <Divider style={{ marginBottom: '32px' }} />
         <ProDescriptions
           title="接口信息"
           request={async () => {
@@ -71,24 +34,16 @@ const Detail: React.FC = () => {
           emptyText={'空'}
           columns={[
             {
-              title: 'API端点名称',
+              title: '接口名称',
               dataIndex: 'name',
             },
             {
-              title: 'API端点描述',
+              title: '接口描述',
               dataIndex: 'description',
-              valueType: 'textarea',
-            },
-            {
-              title: '调用频率限制',
-              dataIndex: 'rateLimit',
-              hideInForm: true,
-              hideInSearch: true,
             },
             {
               title: '状态',
               dataIndex: 'status',
-              hideInForm: true,
               valueEnum: {
                 0: {
                   text: '未生效',
@@ -101,53 +56,24 @@ const Detail: React.FC = () => {
               },
             },
             {
-              title: '创建时间',
-              dataIndex: 'updateTime',
-              valueType: 'dateTime',
-            },
-          ]}
-        ></ProDescriptions>
-        <Divider style={{ marginBottom: '32px' }} />
-        <ProDescriptions
-          title="请求参数"
-          request={async () => {
-            return Promise.resolve({
-              success: true,
-              data: data,
-            });
-          }}
-          style={{ marginBottom: '32px' }}
-          emptyText={'空'}
-          columns={[
-            {
-              title: 'API端点名称',
-              dataIndex: 'name',
+              title: '接口地址',
+              dataIndex: 'url',
             },
             {
-              title: 'API端点描述',
-              dataIndex: 'description',
-              valueType: 'textarea',
+              title: '请求方式',
+              dataIndex: 'method',
+            },
+            {
+              title: '支持协议',
+              dataIndex: 'url',
+            },
+            {
+              title: '返回格式',
+              dataIndex: 'url',
             },
             {
               title: '调用频率限制',
               dataIndex: 'rateLimit',
-              hideInForm: true,
-              hideInSearch: true,
-            },
-            {
-              title: '状态',
-              dataIndex: 'status',
-              hideInForm: true,
-              valueEnum: {
-                0: {
-                  text: '未生效',
-                  status: 'Default',
-                },
-                1: {
-                  text: '生效中',
-                  status: 'Processing',
-                },
-              },
             },
             {
               title: '创建时间',
@@ -156,7 +82,17 @@ const Detail: React.FC = () => {
             },
           ]}
         ></ProDescriptions>
+
         <Divider style={{ marginBottom: '32px' }} />
+
+        <p style={titleStyle}>请求参数</p>
+        <ProTable
+          className={'interface-detail-table'}
+          options={false}
+          search={false}
+          style={{ paddingLeft: '-24px', paddingRight: '-24px' }}
+        ></ProTable>
+
         <ProDescriptions
           title="返回示例"
           request={async () => {
